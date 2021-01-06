aa067c85eef9c73b51eeaffa33e4fd09bfc63a08
Hirenkumar Khant <hr266981@dal.ca>
2021-01-06 00:00:52 -0400

    added csv data parsing for graph f and mapped each column to an array

--- d3_f.js
+++ d3_f.js
@@ -1,4 +1,15 @@
 // visualization for Are Your Organization's Leaders Receptive to Emerging Tech?
+//reading csv file
+d3.csv("Data/data_f.csv").then(data => {
+  data.forEach(d => {
+    d.Percentage = +d.Percentage;
+  });
+  render(data);
+});
+
+// creating empty arrays to be populated with data further
+var percentage = [],
+  scale = [];
 
 //selecting root svg
 const svg = d3.select("svg");
@@ -13,32 +24,40 @@ graf.attr("transform", `translate(${positionX},${positionY})`);
 // colors for all 4 circles
 var colors = ["#ec7936", "#fca24c", "#fecd88", "#fdeac2"];
 // manipulating and transforming the svg circles already created on the base graph
-var circle = d3.selectAll(".circleF")
+var circle = d3
+  .selectAll(".circleF")
   .style("fill", "#ffffff")
   .style("stroke", function (d, i) {
-    return colors[i];   
+    return colors[i];
   })
   .style("stroke-width", "2px")
   .attr("transform", "translate(143, 476)");
 
-// percentage data to be displayed
-var data = [23, 50, 17, 7];
 // positional co-ordinates for the percentages
-var dataX = [117, 147, 177, 207];
+var dataX = [117, 147.5, 178, 208.5];
 // appending percentages to the graph
-var perc = graf.selectAll(".text")
-  .data(data)
-  .enter()
-  .append("text")
-  .text(function (d, i) {
-    return data[i] + "%";
-  })
-  .attr("y", 311)
-  .data(dataX)      // x co-ordinates for attr("x")
-  .attr("x", function (d) {
-    return d;
-  })
-  .style("font-size", "8px")
-  .style("fill", "#838383")
-  .style("font-weight", "600")
-  .style("font-family", "sans-serif");
+const render = (data) => {
+  // mapping the data from each column into a separate array
+  data.map(d => {
+    percentage.push(d.Percentage);
+    scale.push(d.Scale);
+  });
+  // creating percentage texts on the graph
+  var perc = graf
+    .selectAll(".text")
+    .data(percentage)
+    .enter() // dynamically creates placeholder references corresponding to the number of values
+    .append("text")
+    .text(d => {
+      return d + "%";
+    })
+    .attr("y", 311)
+    .data(dataX) // x co-ordinates for attr("x")
+    .attr("x", d => {
+      return d;
+    })
+    .style("font-size", "8px")
+    .style("fill", "#838383")
+    .style("font-weight", "600")
+    .style("font-family", "sans-serif");
+};
