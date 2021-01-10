690af2fa4dfcde0576d61623a1509798c7429182
Hirenkumar Khant <hr266981@dal.ca>
2021-01-10 18:15:12 -0400

    reordered function positions for graph d, added highlighting percentages

--- d3_d.js
+++ d3_d.js
@@ -30,7 +30,7 @@ var colors = [
   "#fbf6e0",
 ];
 //colors for hovering effect from legend
-var colorarr = [
+var colorArr = [
   [
     "#bfff00",
     "#f78b4baa",
@@ -122,12 +122,11 @@ const render = (data) => {
     .data(data)
     .enter()
     .append("rect")
-    .attr("id", function (d, i) {
-      return "graph-bar-d" + (i + 1);
-    })
-    .attr("class", function (d, i) {
-      return "graph-elements-d graph-bars-d graph-elements-d" + (i + 1);
-    })
+    .attr("id", (d, i) => "graph-bar-d" + (i + 1))
+    .attr(
+      "class",
+      (d, i) => "graph-elements-d graph-bars-d graph-elements-d" + (i + 1)
+    )
     .attr("x", (d) => xOffset(d))
     .attr("width", xScale.bandwidth())
     .attr("height", 0)
@@ -142,66 +141,6 @@ const render = (data) => {
     .attr("height", (d) => height - yOffset(d))
     .delay((d, i) => i * 100);
 
-  //code snippets to highlight a bar segment from the graph legend beside
-  d3.selectAll(".graph-elements-d")
-    .on("mouseover", mouseoverD)
-    .on("mouseout", mouseoutD);
-
-  // function to highlight relevant bar segment on hover
-  function mouseoverD() {
-    var segmemtId = this.id.slice(-1);
-    bars.attr("fill", (data, i) => colorarr[parseInt(segmemtId) - 1][i]);
-    d3.select("#graph-label-d" + segmemtId)
-      .style("font-weight", 700)
-      .style("fill", "#111111");
-    d3.select("#graph-square-d" + segmemtId).attr("fill", "#bfff00");
-
-    // if(this.classList.contains("graph-elements-d1")){
-    //   // bars.attr("fill", colorId);
-    //   d3.select("#graph-label-d1").style("font-weight",700).style("fill","#111111");
-    //   d3.select("#graph-square-d1").attr("fill", "#bfff00");
-    // }
-    // else if(this.classList.contains("graph-elements-d2")){
-    //   bars.attr("fill", (data, i) => colors2[i]);
-    //   d3.select("#graph-label-d2").style("font-weight",700).style("fill","#111111");
-    //   d3.select("#graph-square-d2").attr("fill", "#bfff00");
-    // }
-    // else if(this.classList.contains("graph-elements-d3")){
-    //   bars.attr("fill", (data, i) => colors3[i]);
-    //   d3.select("#graph-label-d3").style("font-weight",700).style("fill","#111111");
-    //   d3.select("#graph-square-d3").attr("fill", "#bfff00");
-    // }
-    // else if(this.classList.contains("graph-elements-d4")){
-    //   bars.attr("fill", (data, i) => colors4[i]);
-    //   d3.select("#graph-label-d4").style("font-weight",700).style("fill","#111111");
-    //   d3.select("#graph-square-d4").attr("fill", "#bfff00");
-    // }
-    // else if(this.classList.contains("graph-elements-d5")){
-    //   bars.attr("fill", (data, i) => colors5[i]);
-    //   d3.select("#graph-label-d5").style("font-weight",700).style("fill","#111111");
-    //   d3.select("#graph-square-d5").attr("fill", "#bfff00");
-    // }
-    // else if(this.classList.contains("graph-elements-d6")){
-    //   bars.attr("fill", (data, i) => colors6[i]);
-    //   d3.select("#graph-label-d6").style("font-weight",700).style("fill","#111111");
-    //   d3.select("#graph-square-d6").attr("fill", "#bfff00");
-    // }
-    // else if(this.classList.contains("graph-elements-d7")){
-    //   bars.attr("fill", (data, i) => colors7[i]);
-    //   d3.select("#graph-label-d7").style("font-weight",700).style("fill","#111111");
-    //   d3.select("#graph-square-d7").attr("fill", "#bfff00");
-    // }
-  }
-
-  // function to reset the graph color
-  function mouseoutD() {
-    bars.attr("fill", (d, i) => colors[i]);
-    d3.selectAll(".graph-labels-d")
-      .style("font-weight", 500)
-      .style("fill", "#838383");
-    d3.selectAll(".graph-squares-d").attr("fill", (d, i) => colors[i]);
-  }
-
   //appying percentage labels on top of each bar
   const topText = graf.append("g");
   const texts = topText
@@ -212,8 +151,44 @@ const render = (data) => {
     .text((d) => yValue(d) + "%")
     .attr("x", (d) => xOffset(d) + xScale.bandwidth() / 4)
     .attr("y", (d) => yOffset(d) - 3)
+    .attr("id", (d, i) => "graph-percentage-d" + (i + 1))
+    .attr(
+      "class",
+      (d, i) => "graph-elements-d graph-percentage-d graph-elements-d" + (i + 1)
+    )
     .attr("fill", "#777777")
     .attr("font-size", 6)
     .attr("font-weight", 600)
     .attr("font-family", "sans-serif");
+
+  //code snippets to highlight a bar segment from the graph legend beside
+  d3.selectAll(".graph-elements-d")
+    .on("mouseover", mouseoverD)
+    .on("mouseout", mouseoutD);
+
+  // function to highlight relevant bar segment on hover
+  function mouseoverD() {
+    var segmemtId = this.id.slice(-1); //represents the id for the graph segment (1,2,3,...,7)
+    bars.attr("fill", (data, i) => colorArr[parseInt(segmemtId) - 1][i]); //to change the color of the bars
+    d3.select("#graph-label-d" + segmemtId) //to highlight the label fonts
+      .style("font-weight", 700)
+      .style("fill", "#111111");
+    d3.select("#graph-percentage-d" + segmemtId) //to highlight the percentage fonts
+      .style("font-weight", 700)
+      .style("fill", "#111111");
+    d3.select("#graph-square-d" + segmemtId).attr("fill", "#bfff00"); //to highlight the legend square
+  }
+
+  // function to reset the graph color
+  function mouseoutD() {
+    console.log("mouseover");
+    bars.attr("fill", (d, i) => colors[i]); //to reset the color of the bars
+    d3.selectAll(".graph-labels-d") //to reset the label fonts
+      .style("font-weight", 500)
+      .style("fill", "#838383");
+    d3.selectAll(".graph-percentage-d") //to reset the percentage fonts
+      .style("font-weight", 600)
+      .style("fill", "#777777");
+    d3.selectAll(".graph-squares-d").attr("fill", (d, i) => colors[i]); //to reset the legend square
+  }
 };
